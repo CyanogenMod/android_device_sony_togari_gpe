d1288efb1792df640bdfef3a5c24856825d796a4
Chirayu Desai <chirayudesai1@gmail.com>
2014-02-03 15:30:29 +0530

    dedupe makefile rules
    * What we had in togari_gpe.mk is the same as togari.mk, so
      just include that rather than copy-paste the rules.
    
    Change-Id: I8e5c2e8457f9e4662c2d18efeefaab96c11327aa

--- togari_gpe.mk
+++ togari_gpe.mk
@@ -12,49 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-COMMON_PATH := $(LOCAL_PATH)/../togari
-
 # Include non-opensource parts
 # We call this first because togari_gpe contains some binaries
 # which are also present in togari or rhine
-# By calling togari_gpe first, the other duplicates are ignored
+# By calling togari_gpe first, the other duplicates (as in filename)
+# are ignored
 $(call inherit-product, vendor/sony/togari_gpe/togari_gpe-vendor.mk)
-$(call inherit-product, vendor/sony/togari/togari-vendor.mk)
 
-# Inherit the fusion-common definitions
-$(call inherit-product, device/sony/rhine-common/rhine.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/sony/togari/overlay
-
-# These are the hardware-specific features
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
-
-# This device is xhdpi.  However the platform doesn't
-# currently contain all of the bitmaps at xhdpi density so
-# we do this little trick to fall back to the hdpi version
-# if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := large hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# Device specific init
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/init.device.rc:root/init.device.rc
-
-# USB function switching
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc
-
-# Vold
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/etc/vold.fstab:system/etc/vold.fstab
-
-# Device specific part for two-stage boot
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/recovery/bootrec-device:recovery/bootrec-device
-
-# call dalvik heap config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+# Inherit the togari definitions
+LOCAL_PATH := device/sony/togari
+include device/sony/togari/togari.mk
+LOCAL_PATH := $(call my-dir)
