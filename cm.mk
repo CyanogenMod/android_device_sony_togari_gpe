7325706d64b189db8ad9b7c9eb9ccef5ea45662a
Chirayu Desai <cdesai@cyanogenmod.org>
2014-06-08 10:34:00 +0530

    update to 4.4.3 build description and fingerprint
    Change-Id: I6fc8597ac47cff4c335bc03c114950db089258c7

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/togari_gpe/full_togari_gpe.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6806_GPe BUILD_FINGERPRINT=Sony/C6806_GPe/C6806:4.4.2/KOT49H.S2/2052:user/release-keys PRIVATE_BUILD_DESC="C6806-user 4.4.2 KOT49H.S2 2052 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6806_GPe BUILD_FINGERPRINT=Sony/C6806_GPe/C6806:4.4.3/KTU84L.S1/3056:user/release-keys PRIVATE_BUILD_DESC="C6806-user 4.4.3 KTU84L.S1 3056 test-keys"
 
 PRODUCT_NAME := cm_togari_gpe
 PRODUCT_DEVICE := togari_gpe
